f87448353f4fa6600d649a05b542bbd2a4e1e71d
paulo2606 <paulohpereira@outlook.com.br>
2026-07-10 02:05:49 -0300

    feat: adicionar Swagger para testar os endpoints

--- src/AgendamentoClinica.Api/Program.cs
+++ src/AgendamentoClinica.Api/Program.cs
@@ -1,8 +1,41 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(opcoes =>
+{
+    opcoes.SwaggerDoc("v1", new OpenApiInfo { Title = "AgendamentoClinica.Api", Version = "v1" });
+
+    opcoes.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Cole aqui só o token (sem o prefixo 'Bearer ')."
+    });
+    opcoes.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 app.MapControllers();
